b98b8ff3a6ee4377ad96c0505e03257a7e4f63c9
Daniel Bodas <danielbodas.14@gmail.com>
2018-03-20 09:13:33 +0100

    Add files via upload

---
+++ Prueba2/src/main/java/Main/Sp07Application.java
@@ -0,0 +1,13 @@
+package Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp07Application.class, args);
+    }
+
+}
